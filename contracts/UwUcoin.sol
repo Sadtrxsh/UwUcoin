f11ab4e027fc311df1ce80e79d5761ac682e65ac
Ø <79362520+legitnull@users.noreply.github.com>
2021-03-13 13:53:41 -0700

    added transfer function

--- contracts/UwUcoin.sol
+++ contracts/UwUcoin.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.22;
+ pragma solidity ^0.4.22; //update to 9 eveT(a)
 
 contract UwUcoin {
   string public name = "UwU Coin";
@@ -6,23 +6,33 @@ contract UwUcoin {
   string public standard = "UwU Coin v1.0";
   uint256 public totalSupply;
 
-  //event Transfer(
-    //address indexed _from,
-    //address indexed _to,
-    //uint _value
-  //);
+  event Transfer(
+    address indexed _from,
+    address indexed _to,
+    uint _value
+  );
 
-  //event Approval(
-    //address indexed _owner,
-    //address indexed _spender,
-    //uint256 _value
-  //);
+  event Approval(
+    address indexed _owner,
+    address indexed _spender,
+    uint256 _value
+  );
 
-  //mapping(address => uint256) public balanceOf;
-  //mapping(address => mapping(address => uint256)) public allowance;
+  mapping(address => uint256) public balanceOf;
+  mapping(address => mapping(address => uint256)) public allowance;
 
-  function UwUcoin () public {
-    //balanceOf[msg.sender] = _initalSupply;
-    totalSupply = 1000000; //_initalSupply;
+  function UwUcoin (uint256 _initalSupply) public {
+    balanceOf[msg.sender] = _initalSupply;
+    totalSupply = _initalSupply;
+  }
+  function transfer(address _to, uint256) public {
+    require(balanceOf[msg.sender] >= _value);
+
+    balanceOf[msg.sender] -= _value;
+    balanceOf[_to] += _value;
+
+    Transfer(msg.sender, _to, _value);
+
+    return true;
   }
 }
